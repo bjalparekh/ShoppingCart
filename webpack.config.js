20d022d4092a0c2c4540000a6198715af8f71254
Bijal Parekh <Bijal_Parekh@external.mckinsey.com>
2016-11-28 12:45:02 +0530

    Bijal | Webpack production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,53 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    context: __dirname,
-    entry: {
-			js:'./src/javascripts/app.js',
-			css: './src/assets/stylesheets/main.scss'
-		},
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'app.js'
-    },
-    resolve: {
-        extensions: ['', '.js', '.json', '.jsx']
-    },
-    stats: {
-        color: true,
-        reasons: true
-    },
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            loader: 'babel',
-            query: {
-                presets: ['es2015', 'react']
-            }
-        },
-				{
-					test: /\.scss$/,
-					loader: ExtractTextPlugin.extract('style','css!sass'),
-				},
-				{ test: /\.json$/, loader: "json-loader" }
-			],
-    },
-		plugins: [
-        new ExtractTextPlugin('main.css', {allChunks: false}),
-				new webpack.optimize.UglifyJsPlugin({
-					compress: { warnings: false },
-					include: /\.min\.js$/
-				}),
-				new OptimizeCssAssetsPlugin({
-					assetNameRegExp: /\.min\.css$/,
-					cssProcessorOptions: { discardComments: { removeAll: true } }
-				})
-    ]
+	context: __dirname,
+	entry: {
+		js: './src/javascripts/app.js',
+		css: './src/assets/stylesheets/main.scss'
+	},
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'app.min.js'
+	},
+	resolve: {
+		extensions: ['', '.js', '.json', '.jsx']
+	},
+	stats: {
+		color: true,
+		reasons: true
+	},
+	module: {
+		loaders: [{
+			test: /\.jsx?$/,
+			loader: 'babel',
+			query: {
+				presets: ['es2015', 'react']
+			}
+		}, {
+			test: /\.scss$/,
+			loader: ExtractTextPlugin.extract('style', 'css!sass'),
+		}, {
+			test: /\.json$/,
+			loader: "json-loader"
+		}],
+	},
+	plugins: [
+		new ExtractTextPlugin('main.css', {
+			allChunks: false
+		}),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production')
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false
+			},
+			include: /\.min\.js$/
+		}),
+	]
 }

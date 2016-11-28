9ef517dea6e290942c18484080c6f7717df6938e
Bijal Parekh <Bijal_Parekh@external.mckinsey.com>
2016-11-28 14:32:09 +0530

    Bijal | Add accessibility to input

--- src/javascripts/components/CartCoupon.jsx
+++ src/javascripts/components/CartCoupon.jsx
@@ -5,12 +5,13 @@ export default class CartCoupon extends Component {
 	render() {
 		return (
 			<div className="cart-coupon">
-				<form action="post">
+				<form action="post" id="coupon">
 					<fieldset>
 						<legend className="cart-coupon--text">Enter promotion code<br /> or gift card:</legend>
 						<div className="cart-coupon-action">
-							<label for="couponcode"></label>
-							<input type="text"  className="cart-coupon--input" id="couponcode" />
+							<label for="couponcode">
+								<input type="text"  className="cart-coupon--input" id="couponcode" />
+							</label>
 							<button className="btn btn-secondary cart-coupon--btn">Apply</button>
 						</div>
 					</fieldset>
